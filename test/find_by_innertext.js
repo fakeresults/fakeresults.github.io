50b631bbb1de35c1e076b74724da08533598f018
generatorinchief <77995324+generatorinchief@users.noreply.github.com>
2021-01-28 18:21:36 +0200

    Create find_by_innertext.js

---
+++ test/find_by_innertext.js
@@ -0,0 +1,6 @@
+function contains(selector, text) {
+  var elements = document.querySelectorAll(selector);
+  return Array.prototype.filter.call(elements, function(element){
+    return RegExp(text).test(element.textContent);
+  });
+}
